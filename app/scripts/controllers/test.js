3f9dba49e5155e9486607fa60abbcefc1d39a06f
langz <anderslangseth@hotmail.com>
2014-09-25 13:57:46 +0200

    Added a test Controller for showing to a friend

---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name divingInToDirectivesApp.controller:TestCtrl
+ * @description
+ * # TestCtrl
+ * Controller of the divingInToDirectivesApp
+ */
+angular.module('divingInToDirectivesApp')
+  .controller('TestCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
