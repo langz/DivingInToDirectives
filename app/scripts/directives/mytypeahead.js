e165293fe71e0b845668177ea68f691e65c44fa6
langz <anderslangseth@hotmail.com>
2014-10-14 23:52:41 +0200

    Changed mytypeahead

--- app/scripts/directives/mytypeahead.js
+++ app/scripts/directives/mytypeahead.js
@@ -9,7 +9,7 @@
 angular.module('divingInToDirectivesApp')
 .directive('mytypeahead', function () {
   return {
-    template:'<input type="text" ng-model="myDirectiveVar" typeahead="state for state in states | filter:$viewValue | limitTo:8" class="form-control">',
+    template:'<input type="text" ng-model="myDirectiveVar" typeahead="state as state.name for state in states | filter:$viewValue | limitTo:8" typeahead-template-url="customTemplate.html" class="form-control">',
     restrict: 'AE',
     scope: {
       myDirectiveVar: '=',
@@ -17,12 +17,7 @@ angular.module('divingInToDirectivesApp')
     replace: true,
     link: function (scope, elem, attr) {
 
-      scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
-      'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-      'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina',
-      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
-      'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+      scope.states = [{name: 'Alabama', value:12}, {name: 'Alaska', value: 13}];
     }
   };
 });
